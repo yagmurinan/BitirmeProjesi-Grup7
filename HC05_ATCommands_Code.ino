84c4315a625dcac5f7493adf0c3fb34864c893ec
yagmurinan <108583790+yagmurinan@users.noreply.github.com>
2022-07-03 23:05:12 +0300

    Add files via upload

---
+++ HC05_ATCommands_Code.ino
@@ -0,0 +1,28 @@
+/*  Bluetooth AT Commands
+ *  Uğur ALTINIŞIK
+ *  Bu program Bluetooth Modulu ayar modunun yuklenmesini ve degistirilmesine olanak saglanmasini icerir.
+ */
+
+#include <SoftwareSerial.h> //haberlesme kutuphanesi tanimlanmistir.
+SoftwareSerial BluetoothSw(10, 11); //Arduino UNO'nun 10 ve 11. pinleri sirasi ile Arduino UNO'nun RX TX pini olarak tanimlanmistir
+
+void setup() 
+{
+  Serial.begin(9600); //Serial Port Baud Rate degeri tanimlanmistir.
+  Serial.println("Enter AT Commands:"); //Serial Port Yazi yazdirilmistir
+  BluetoothSw.begin(38400); //Bluetooth modul default Baud Rate degeri atanmistir
+}
+void loop() 
+{
+  if(BluetoothSw.available()) //Bluetoothtan veri geliyorsa
+  {
+    Serial.write(BluetoothSw.read()); //veriyi okur ve Serial Port'a yazar
+  }
+  if(Serial.available()) //Serial Port'tan veri geliyorsa
+  {
+    BluetoothSw.write(Serial.read()); //veriyi okur ve Bluetooth ile gonderir
+  }
+}
+
+//Ayar modu icin Serial Porta AT+<KOMUT>? ile mevcut bilgi ogrenilir, mevcut bilgi degistirilecekse AT+<KOMUT>=<DEGER> ile durum degistirilir
+//ORNEK: AT+NAME=BitirmeGrup7AA -> Bluetooth modulunun adi BitirmeGrup7AA olur
